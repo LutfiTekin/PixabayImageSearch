40351e4c5f88875a56116b9d77cd78751bf6990e
Lütfi Tekin <lutfi-tekin@outlook.com>
2021-11-21 00:10:19 +0300

    Fetch api key from firebase remote config

---
+++ app/src/main/java/tekin/lutfi/pixabay/utils/BindingAdapters.kt
@@ -0,0 +1,11 @@
+package tekin.lutfi.pixabay.utils
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+
+
+@BindingAdapter("load_image")
+fun ImageView.loadImage(image: String){
+    this.load(image)
+}
