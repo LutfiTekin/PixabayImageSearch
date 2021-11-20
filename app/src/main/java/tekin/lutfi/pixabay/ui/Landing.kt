c062daa0568889aa48d0864e2ce5a18449946893
Lütfi Tekin <lutfi-tekin@outlook.com>
2021-11-21 00:10:45 +0300

    cleanup

---
+++ app/src/main/java/tekin/lutfi/pixabay/ui/Landing.kt
@@ -0,0 +1,46 @@
+package tekin.lutfi.pixabay.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import tekin.lutfi.pixabay.R
+import tekin.lutfi.pixabay.databinding.ActivityLandingBinding
+import tekin.lutfi.pixabay.databinding.ActivityMainBinding
+
+class Landing : AppCompatActivity() {
+
+    private var binding: ActivityLandingBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        fetchRemoteConfigAndRedirect()
+    }
+
+
+    /**
+     * Fetch api from firebase remote config
+     */
+    private fun fetchRemoteConfigAndRedirect() {
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        Firebase.remoteConfig.apply {
+            setConfigSettingsAsync(configSettings)
+            setDefaultsAsync(R.xml.remote_config)
+            fetchAndActivate().addOnCompleteListener {
+                startActivity(Intent(this@Landing, MainActivity::class.java))
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
