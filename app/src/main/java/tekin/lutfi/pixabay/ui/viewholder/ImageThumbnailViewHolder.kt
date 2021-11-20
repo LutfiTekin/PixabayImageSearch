fc33d758a8776ba6b2b0a254120da0cdc95f5b73
Lütfi Tekin <lutfi-tekin@outlook.com>
2021-11-20 23:30:00 +0300

    viewholder

---
+++ app/src/main/java/tekin/lutfi/pixabay/ui/viewholder/ImageThumbnailViewHolder.kt
@@ -0,0 +1,15 @@
+package tekin.lutfi.pixabay.ui.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import tekin.lutfi.pixabay.data.PixabayImage
+import tekin.lutfi.pixabay.databinding.ItemThumbnailBinding
+
+class ImageThumbnailViewHolder internal constructor(
+    private val binding: ItemThumbnailBinding
+    ): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(image: PixabayImage?){
+
+        }
+
+}
